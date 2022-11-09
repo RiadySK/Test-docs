e82a47292e7f5e1837518d8eabe4df5827c47c78
Riady Sastra Kusuma <riady.s.kusuma@gdplabs.id>
2022-11-09 13:10:43 +0700

    Fix build error

--- 3rdparty.d.ts
+++ 3rdparty.d.ts
@@ -1,18 +1,21 @@
 /**
  * Define third party global Window
  */
-const AppleID: any
-const FB: any
-const googletag: any
-const gapi: any
-const ga: any
-
-interface Window {
-  fbAsyncInit: any
-  dataLayer: any
-  gapi: any
-  googletag: any
-  FB: any
-  AppleID: any
-  ga: any
-}
+ const AppleID: any
+ const FB: any
+ const googletag: any
+ const grecaptcha: any
+ const gapi: any
+ const ga: any
+ 
+ interface Window {
+   fbAsyncInit: any
+   dataLayer: any
+   gapi: any
+   googletag: any
+   grecaptcha: any
+   FB: any
+   AppleID: any
+   ga: any
+ }
+ 
